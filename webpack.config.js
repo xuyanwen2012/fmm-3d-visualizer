948ee00e6f6a3e0604241012548bf43e8ea2cd1a
Ivan Xu <info@chivalrystudio.com>
2021-08-01 13:47:37 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({'title': '3D FMM Viewer'}),
+  ],
+};
