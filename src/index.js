8665fe0c46b599c116e4cc403712dbab77ee7eb4
Ivan Xu <info@chivalrystudio.com>
2021-08-02 18:07:38 -0700

    change post-processing

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer';
 import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js';
-import {BloomPass} from 'three/examples/jsm/postprocessing/BloomPass.js';
+import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import MainScene from './main_scene';
 
 // Init
@@ -18,7 +18,7 @@ const mainScene = new MainScene();
 const clock = new Clock();
 
 scene.add(mainScene);
-scene.fog = new FogExp2(0x000000, 0.0001);
+// scene.fog = new FogExp2(0x000000, 0.0001);
 
 const camera = new PerspectiveCamera(
     55, window.innerWidth / window.innerHeight, 2, 50000);
@@ -26,10 +26,12 @@ const renderer = new WebGLRenderer({antialias: true});
 const control = new OrbitControls(camera, renderer.domElement);
 
 const renderModel = new RenderPass(scene, camera);
-const effectBloom = new BloomPass(0.75);
+const effectBloom = new UnrealBloomPass();
 const composer = new EffectComposer(renderer);
+
 composer.addPass( renderModel );
 composer.addPass( effectBloom );
+
 
 // camera
 camera.position.z = 1000;
